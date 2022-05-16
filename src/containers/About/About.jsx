62e23027c1410021cd86b6d76353292215a2f460
nomanmajeed <nomanmajeed986@gmail.com>
2022-05-16 21:32:13 +0500

    Work Section Done

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Children } from 'react';
+import React, { useState, useEffect} from 'react';
 import { motion } from 'framer-motion';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
