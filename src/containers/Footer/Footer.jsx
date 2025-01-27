5df5e20f77e6aabc46dd838584dcd1f423f00f8c
Noman Majeed <nomanmajeed986@gmail.com>
2025-01-27 23:51:30 +0500

    feat: footer component

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import './Footer.scss'
-
+import React from 'react';
+import './Footer.scss';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="footer__content">
+        <div className="footer__left">
+          <p>Built with React & SCSS</p>
+        </div>
+        
+        <div className="footer__right">
+          <p>© {new Date().getFullYear()} Noman Majeed. All rights reserved.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
