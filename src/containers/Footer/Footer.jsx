af1bc0c0ee5d31c69aedd4ecd81f89afd16cef7b
Noman Majeed <nomanmajeed986@gmail.com>
2025-01-28 00:03:11 +0500

    feat: footer section

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import './Footer.scss';
+import React from "react";
+import "./Footer.scss";
 
 const Footer = () => {
   return (
     <div className="footer app__flex">
       <div className="footer__content">
-        <div className="footer__left">
-          <p className="p-text">Built with React & SCSS</p>
+        <div className="footer__text">
+          <p> {new Date().getFullYear()} Noman Majeed</p>
+          <p>Built with React & Passion</p>
         </div>
-        
-        <div className="footer__right">
-          <p className="p-text">© {new Date().getFullYear()} Noman Majeed. All rights reserved.</p>
+        <div className="footer__socials">
+          <a
+            href="https://github.com/nomanmajeed"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          <a
+            href="https://linkedin.com/in/nomanmajeed"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
         </div>
-      </div>
-      <div className="copyright">
-        <p className="p-text">@2023 NOMAN</p>
-        <p className="p-text">All rights reserved</p>
       </div>
     </div>
   );
